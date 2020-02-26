48695d8d52140c74fa44142f13f4a57649329378
pohsun <po-hsun@ampostech.com>
2020-02-27 01:31:29 +0800

    rename-file script

---
+++ rename-file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $1
+
+if [[ $1 == '' ]]
+then
+  echo "should provide the directory path"
+  exit 1
+fi
+
+IFS=$'\n'
+
+for filename in $(ls -d *)
+do
+    unset IFS
+    result=""
+    for word in $filename
+    do
+      first=$(echo ${word:0:1} | tr '[:lower:]' '[:upper:]')
+      result+="${first}${word:1} "
+    done
+    result=$(sed 's/\\s*$//' <<< ${result})
+
+    mv "${filename}/" "${result}"
+done
